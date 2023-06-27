95a88e0dd06ae9524d2052db344ec6829ec6d10e
guneykilicel <kilicelguney@gmail.com>
2023-06-27 17:50:35 +0300

    generatedPassword

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  password = '';
   length : number = 0;
   
   includeLetters:boolean = false;
@@ -32,10 +33,29 @@ export class AppComponent {
   }
 
   buttonClick() {
-    console.log(`Değerlerim:
-    Letters: ${this.includeLetters}
-    Numbers: ${this.includeNumbers}
-    Symbols: ${this.includeSymbols}
-    `);
+    const numbers='1234567890';
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const symbols = '!"#$%&()*+,-./:;<=>?@[]^_{|}~';
+
+    let validChars = '';
+
+    if(this.includeLetters) {
+      validChars += letters;
+    }
+    if(this.includeNumbers) {
+      validChars += numbers;
+    }
+    if(this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+    for(let i=0; i<this.length; i++) {
+      const index = Math.floor(Math.random()*validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+
   }
 }
